539c3660d44f3396ac0fd7f1f40d7adff0d13cda
yujianfu <yujianfu@passiontec.cn>
2017-05-20 15:58:22 +0800

    oschina代码迁移

--- src/test/java/com/imooc/GirlApplicationTests.java
+++ src/test/java/com/imooc/GirlApplicationTests.java
@@ -2,8 +2,11 @@ package com.imooc;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -11,6 +14,7 @@ public class GirlApplicationTests {
 
 	@Test
 	public void contextLoads() {
+
 	}
 
 }
